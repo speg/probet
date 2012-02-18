9baf8544843e46311991c37bf662ff552ff4cc9e
Steve <steve@chide.it>
2012-02-18 12:05:25 -0500

    cleaning up
    refactoring into functions and pretty printing

--- probet/test.py
+++ probet/test.py
@@ -3,33 +3,53 @@ import urllib2
 import urllib
 from bs4.element import NavigableString
 
-url = "http://www.nhl.com/ice/m_standings.htm?type=LEA&season=20112012"
-url = 'http://proline.olg.ca/prolineEvents.do'
 
-form = {'selectedSportId':'HKY',
-    'selectedTimeTab':'thisWeek',
-    'language':'',
-    'wagerSelect':'2'
-    }
-data = urllib.urlencode(form)
+def getTeams(debug=False, save=False):
+    #get standings and return soup
+    
+    if debug:
+        html = open('teams.html').read()
+    else:
+        url = "http://www.nhl.com/ice/m_standings.htm?type=LEA&season=20112012"
+        result = urllib2.urlopen(url)
+        html = result.read()
 
-#req = urllib2.Request(url, data)
-#response = urllib2.urlopen(req)
-#the_page = response.read()
-try:
-  #result = urllib2.urlopen(url)
-  html = open('odds.html')#result.read()
-  soup = BeautifulSoup(html)
-except urllib2.URLError, e:
-  print e
+    if save:
+        f = open('teams.html','w')
+        f.write(html)
+        f.close()
+
+    return BeautifulSoup(html)
+
+def getOdds(debug=False, save=False):
+    #get odds and return soup
+    
+    if debug:
+        html = open('odds.html').read()
+    else:
+        url = 'http://proline.olg.ca/prolineEvents.do'
+        form = {'selectedSportId':'HKY',
+            'selectedTimeTab':'thisWeek',
+            'language':'',
+            'wagerSelect':'2'
+            }
+        data = urllib.urlencode(form)
+        req = urllib2.Request(url, data)
+        response = urllib2.urlopen(req)
+        html = response.read()
+    
+    if save:
+        f = open('odds.html','w')
+        f.write(html)
+        f.close()
+
+    return BeautifulSoup(html)
+
 
 
 def parseStandings(soup):
     table = soup.find_all('table')
-
-
     teams = {}
-
 
     for i in table[1].children:
         tds = i.find_all('td')
@@ -42,6 +62,21 @@ def parseStandings(soup):
         teams[tds[1].text.strip()] = {'pts': pts}
 
     return teams
+
+def translate(x):
+    #translate proline abreviations to NHL ones.
+    t = {   'LA':'LAK',
+            'CLB':'CBJ',
+            'TB':'TBL',
+            'WAS':'WSH',
+            'SJ':'SJS',
+            'NAS':'NSH',
+            'NJ':'NJD'
+    }
+
+    if x in t: x = t[x]
+
+    return x
 
 def parseOdds(soup):
     table = soup.find_all('table')
@@ -64,27 +99,33 @@ def parseOdds(soup):
                         odds.append(bj[1])
                     elif len(bj) == 1:
                         odds.append(bj[0])
-                ODDS.append([odds[4], odds[6], float(odds[8]), float(odds[10])])
+                ODDS.append([translate(odds[4]), translate(odds[6]), float(odds[8]), float(odds[10]),float(odds[7]),float(odds[11])])
 
             # for j, td in enumerate(tds):
             #     print '*',i, td.text.strip()
             #print tds[8].text.strip()
     return ODDS
 
-odds = parseOdds(soup)
+def printOdds(odds):
+    #pretty print of odds table
+    #currentley: visitor, home, v, h, v+, h+
+    print '### PROBET PROLINE ###'
+    print ''
+    print 'Visitor\tHome\tV\tH\tV+\tH+'
+    for i in odds:
+        print '%s\t%s\t%s\t%s\t%s\t%s' % tuple(i)
 
 
-try:
-  #result = urllib2.urlopen(url)
-  html = open('data.html')#result.read()
-  soup = BeautifulSoup(html)
-except urllib2.URLError, e:
-  print e
 
-teams = parseStandings(soup)
+DEBUG = True    #debug reads local files
+SAVE = False    #save writes local files
 
-#print odds
-#print teams
+odds = parseOdds(getOdds(save=SAVE, debug=DEBUG))
+teams = parseStandings(getTeams(save=SAVE, debug=DEBUG))
+
+
+printOdds(odds)
+
 
 favs = []
 #for each game, find favourite:
@@ -101,16 +142,11 @@ for game in odds:
 favs.sort(key=lambda x: x[2],reverse=True)
 
 
+
+
 for game in favs:
     #determine difference in standings of the two teams:
-    if game[0] == 'LA': game[0] = 'LAK'
-    if game[0] == 'CLB': game[0] = 'CBJ'
-    if game[1] == 'LA': game[1] = 'LAK'
-    if game[1] == 'CLB': game[1] = 'CBJ'
-    if game[0] == 'TB': game[0] = 'TBL'
-    if game[1] == 'TB': game[1] = 'TBL'
-    if game[0] == 'WAS': game[0] = 'WSH'
-    if game[1] == 'WAS': game[1] = 'WSH'
+
     pd = teams[game[0]].get('pts') - teams[game[1]].get('pts')
     print '%s leads %s by %s (%s)' % (game[0], game[1], pd,round(game[2],2))
 
